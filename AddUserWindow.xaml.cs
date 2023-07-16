26421f347da25c443c29b11ce8a0295180999959
PPHcode <139691889+PPHcode@users.noreply.github.com>
2023-07-16 23:17:38 +0530

    Add project files.

---
+++ AddUserWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project01
+{
+    /// <summary>
+    /// Interaction logic for AddUserWindow.xaml
+    /// </summary>
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow(AddUserVM vm)
+
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.CloseAction = () => Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+                  }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Project01.Model
+{
+    public class User
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public BitmapImage Image { get; set; }  
+
+        public string  DateOfBirth{ get; set; }
+        public double Gpa { get; set; }
+
+        public String ImagePath
+        {
+            get { return $"/Model/Images/{Image}"; }
+        }
+
+       
+        public User(int age, string firstName, string lastName, string dateOfBirth,BitmapImage  image,double gpa )
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Image = image;
+            Gpa = gpa;
+        }
+
+        public User(int v)
+        {
+        }
+
+        public User()
+        {
+        }
+    }
+}
